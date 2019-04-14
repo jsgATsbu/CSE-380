89da5fa769752c1645ab99d685ba843be45d87c6
Shenggui Jin <shenggui.jin@stonybrook.edu>
2019-04-14 14:12:31 -0400

    Add GUI

--- js/Boot.js
+++ js/Boot.js
@@ -11,6 +11,12 @@ TheLegendOfMeta.Boot.prototype = {
     this.load.image('selectLevelBG','assets/images/SelectLevel.png');
     this.load.image('settingBG','assets/images/Setting.png');
     this.load.image('helpBG','assets/images/Help.png');
+    this.load.image('Level-1','assets/images/Level1.png');
+    this.load.image('Level-2','assets/images/Level2.png');
+    this.load.image('Level-3','assets/images/Level3.png');
+    this.load.image('Level-4','assets/images/Level4.png');
+    this.load.image('Level-5','assets/images/Level5.png');
+    this.load.image('Level-6','assets/images/Level6.png');
   },
   create: function () {
     this.game.stage.backgroundColor = '#fff';
--- js/SelectLevel.js
+++ js/SelectLevel.js
@@ -6,6 +6,20 @@ TheLegendOfMeta.SelectLevel.prototype = {
     create: function (){
         this.background = this.game.add.tileSprite(0,0,this.game.width,this.game.height, 'selectLevelBG');
 
+        var lvl1 = this.game.add.button(this.game.width*2/10,this.game.height*2/10,'Level-1');
+        var lvl2 = this.game.add.button(this.game.width*5/10,this.game.height*2/10,'Level-2');
+        var lvl3 = this.game.add.button(this.game.width*8/10,this.game.height*2/10,'Level-3');
+        var lvl4 = this.game.add.button(this.game.width*2/10,this.game.height*11/20,'Level-4');
+        var lvl5 = this.game.add.button(this.game.width*5/10,this.game.height*11/20,'Level-5');
+        var lvl6 = this.game.add.button(this.game.width*8/10,this.game.height*11/20,'Level-6');
+
+        lvl1.anchor.setTo(0.5);
+        lvl2.anchor.setTo(0.5);
+        lvl3.anchor.setTo(0.5);
+        lvl4.anchor.setTo(0.5);
+        lvl5.anchor.setTo(0.5);
+        lvl6.anchor.setTo(0.5);
+
         var returnBtn = this.game.add.button(this.game.width/2,this.game.height*5/6,'returnBtn');
         returnBtn.anchor.setTo(0.5);
 

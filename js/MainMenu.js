80942e200be24f35a5b575a93a3b4d6e6826777c
sungyeong <jang.s.g.125@gmail.com>
2019-05-06 22:33:38 -0400

    remove exit, add iced image

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -10,21 +10,19 @@ TheLegendOfMeta.MainMenu.prototype = {
         mainMenuBG.scale.setTo(1.7);
         mainMenuBG.anchor.setTo(0.5);
 
-        var start = this.game.add.button(this.game.width/6, this.game.height*4/5, "startBtn");
-        var selectLevel = this.game.add.button(this.game.width*2/6, this.game.height*4/5, "selectLevelBtn");
-        var setting = this.game.add.button(this.game.width*3/6, this.game.height*4/5, "controlsBtn");
-        var help = this.game.add.button(this.game.width*4/6, this.game.height*4/5, "helpBtn");
-        var exit = this.game.add.button(this.game.width*5/6, this.game.height*4/5, "exitBtn");
+        var start = this.game.add.button(this.game.width/5, this.game.height*4/5, "startBtn");
+        var selectLevel = this.game.add.button(this.game.width*2/5, this.game.height*4/5, "selectLevelBtn");
+        var setting = this.game.add.button(this.game.width*3/5, this.game.height*4/5, "controlsBtn");
+        var help = this.game.add.button(this.game.width*4/5, this.game.height*4/5, "helpBtn");
+        
         start.anchor.set(0.5);
         selectLevel.anchor.set(0.5);
         setting.anchor.set(0.5);
         help.anchor.set(0.5);
-        exit.anchor.set(0.5);
 
         start.onInputDown.add(function(){this.state.start("Level1");},this);
         selectLevel.onInputDown.add(function(){this.state.start("SelectLevel");},this);
         setting.onInputDown.add(function(){this.state.start("Setting");},this);
         help.onInputDown.add(function(){this.state.start("Help");},this);
-        exit.onInputDown.add(function(){this.game.destroy();},this);
     }
 };
